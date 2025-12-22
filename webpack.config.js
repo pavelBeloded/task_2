ef27f901cb19c6dcc9abab57c5dab865b10c11fc
PavelBeloded <beloded_2007@bk.ru>
2025-12-23 02:44:01 +0300

    fix: hot fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    // publicPath: "/",
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
